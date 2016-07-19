871bcea4ce5cef4979a8ca432a9bcfb9984bbfb8
Douglas Lima <douglasproglima@gmail.com>
2016-07-19 12:03:18 -0300

    3.5 - Botões e Links
    <f:param> Utilizado para passar parâmetro pela url
    
    Os componentes <h:outputLink> e <h:link> Não se submetem aos dados,
    diferente do commandLink ou commandButton
    
    <h:commandButton> Gera apenas um html tag input
    
    <h:commandLink> Gera um html e javascript que utiliza a tag img para
    anexar a imagem caso seja utilizado.

---
+++ src/com/douglasproglima/botoeslinks/FormularioBean.java
@@ -0,0 +1,16 @@
+package com.douglasproglima.botoeslinks;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class FormularioBean {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
